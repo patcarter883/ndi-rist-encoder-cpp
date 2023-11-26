09843c5e9afba889db76215d072c022e16d17688
Pat Carter <patcarter883@gmail.com>
2023-11-26 22:45:34 +1100

    Setup cpack

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -1,7 +1,11 @@
 install(
-    TARGETS ndi-rist-encoder ndi-rist-server
-    RUNTIME COMPONENT ndi-rist_Runtime
+    TARGETS ndi-rist-encoder
 )
+
+set(CPACK_PACKAGE_NAME "NDI Rist Encoder")
+set(CPACK_PACKAGE_VENDOR "Pat Carter")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY
+    "NDI to RTMP streamer using AV1 over MPEGTS and RIST")
 
 if(PROJECT_IS_TOP_LEVEL)
   include(CPack)
